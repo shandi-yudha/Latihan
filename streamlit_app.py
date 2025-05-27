00fd7057e4091b89f1f5e34fa7897ffc938e138f
shandi-yudha <shandiyudha8@gmail.com>
2025-05-27 11:56:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ st.write(
 st.image("req.jfif", width=700)
 
 st.title("Stand Power")
-st.header("Aplikasi Mengecek Kekuatan stand")
+st.header("Aplikasi Mengecek Nilai Bilangan Ganjil/Genap")
 angka = st.number_input("Tulis sebuah Angka", value=0, step=1)
 
 if (angka % 2) == 0:
-    st.write(f"{angka} adalah Lemah")
+    st.write(f"{angka} adalah Bilangan Genap")
 else :
-    st.write(f"{angka} adalah Bilangan Kuat")
+    st.write(f"{angka} adalah Bilangan Ganjil")
     
