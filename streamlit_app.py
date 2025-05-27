7cdae3f629c30b16a06772773548948bea9903a3
shandi-yudha <shandiyudha8@gmail.com>
2025-05-27 12:16:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ if (angka % 2) == 0:
     st.write(f"{angka} adalah Bilangan Genap")
 else :
     st.write(f"{angka} adalah Bilangan Ganjil")
-st.write("King otw belajar coding")
+st.title("King otw memperdalam ilmu coding")
     
