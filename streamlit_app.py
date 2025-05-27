61aba1fee8ebd67240a19bd3205c1c6adb6e5400
shandi-yudha <shandiyudha8@gmail.com>
2025-05-27 11:53:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ angka = st.number_input("Tulis sebuah Angka", value=0, step=1)
 if (angka % 2) == 0:
     st.write("f{angka} adalah Lemah")
 else :
-    st.write(f"{angka} adalah Bilangan Kuat")
+    st.write("f{angka} adalah Bilangan Kuat")
     
